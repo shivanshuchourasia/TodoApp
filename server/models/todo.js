ba83538646e4d1949d266d0368ebb30d9072ff6b
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-07-19 18:24:12 +0530

    added models and used express for postman

---
+++ server/models/todo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo};
