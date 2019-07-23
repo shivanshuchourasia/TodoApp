64e839a6b8995376410ac93e205fcb62cccd1119
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-07-23 19:16:03 +0530

    Add GET/todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,12 +1,12 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {User} = require('./../server/models/user');
 
-var id = '6d3146440118c90ef90cf466';
+var id = '5d3146440118c90ef90cf466';
 
 User.findById(id).then((user) => {
   if(!user){
     return console.log('id not found!');
   }
 
-  console.log("User", user);
+  console.log(JSON.stringify(user, undefined, 2));
 }).catch((e) => console.log(e));
