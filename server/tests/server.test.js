4412c27424fbcb2fa7043ba56baa04be728bc32d
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-07-24 00:13:55 +0530

    Add GET/todos/:id tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const request = require('supertest');
 const expect = require('expect');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First todo'
 },
 {
+  _id: new ObjectID(),
   text: 'Second todo'
 }]
 
@@ -72,6 +75,33 @@ describe('GET /todos', () => {
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  })
+})
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var id1 = new ObjectID;
+    request(app)
+      .get(`/todos/${id1.toHexString()}`)
+      .expect(404)
+      .end(done);
+  })
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/1234')
+      .expect(404)
       .end(done);
   })
 })
