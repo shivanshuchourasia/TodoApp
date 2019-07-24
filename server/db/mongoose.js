d135939ea23c442b325d8b024013c0333ffc7613
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-07-24 19:02:30 +0530

    Setup to deploy on Heroku and mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+
+const mlabDB = 'mongodb+srv://Shivanshu:qr782Hep66EDdtZl@todo-app-qhj7g.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mlabDB || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true})
+  .catch((e) => {
+    console.log('error');
+  });
 
 module.exports = {mongoose};
+
+//qr782Hep66EDdtZl
+
+//'mongodb://localhost:27017/TodoApp'
--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
  
 var app = express();
-
+const port = process.env.PORT || 3000;
+ 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -45,8 +46,8 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Connected to the port 3000');
+app.listen(port, () => {
+    console.log(`Started up at port ${port} and process env is ${process.env.NODE_ENV}`);
 });
 
 module.exports = {app};
