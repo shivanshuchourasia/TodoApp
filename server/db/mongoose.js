17aa970f455eb4735928cdd30073d2090f508c33
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-08-03 23:56:44 +0530

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,17 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-require('dotenv').config();
-
-if(!process.env.MLABDB){
-  var db = 'mongodb://localhost:27017/TodoApp';
-}
-else{
-  var db = process.env.MLABDB;
-}
-
-mongoose.connect(db, {useNewUrlParser: true, useFindAndModify: false}).catch((e) => {
-  console.log(e);
+mongoose.connect(process.env.MLABDB, {useNewUrlParser: true, useFindAndModify: false})
+  .catch((e) => {
+    console.log(e);
 });
 
 module.exports = {mongoose};
